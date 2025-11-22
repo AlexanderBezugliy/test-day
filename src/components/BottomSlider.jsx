0eebf1a41ea01257ff6535f4f8955fbf1a74eb81
Alexander <test@tests-MacBook-Air.local>
2025-11-22 13:20:42 +0200

    add (Prettier)

--- src/components/BottomSlider.jsx
+++ src/components/BottomSlider.jsx
@@ -1,89 +1,104 @@
 import React, { useState } from "react";
 import ButtonCircle from "./ui/ButtonCircle";
 
-
 const BottomSlider = () => {
-    const [currentSlide, setCurrentSlide] = useState(1);
+  const [currentSlide, setCurrentSlide] = useState(1);
 
-    const slidesBottom = [
-        {
-            id: 0,
-            buttonName: 'ingredients',
-            left: "/bottom-slider/cart-img2.png",   
-            right: "/bottom-slider/img2.png",
-            title: "Lorem ipsum dolor sit amet consectetur",
-            descr: 'Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.' 
-        },
-        {
-            id: 1,
-            buttonName: 'equipment',
-            left: "/bottom-slider/cart-img1.png",   
-            right: "/bottom-slider/img1.png",
-            title: <>the only limit is <br /> your imagination</>,
-            descr: 'Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.' 
-        },
-        {
-            id: 2,
-            buttonName: 'service',
-            left: "/bottom-slider/cart-img2.png",   
-            right: "/bottom-slider/img2.png",
-            title: 'Lorem ipsum dolor sit amet consectetur',
-            descr: 'Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.' 
-        },
-    
-    ];
+  const slidesBottom = [
+    {
+      id: 0,
+      buttonName: "ingredients",
+      left: "/bottom-slider/cart-img2.png",
+      right: "/bottom-slider/img2.png",
+      title: "Lorem ipsum dolor sit amet consectetur",
+      descr:
+        "Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.",
+    },
+    {
+      id: 1,
+      buttonName: "equipment",
+      left: "/bottom-slider/cart-img1.png",
+      right: "/bottom-slider/img1.png",
+      title: (
+        <>
+          the only limit is <br /> your imagination
+        </>
+      ),
+      descr:
+        "Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.",
+    },
+    {
+      id: 2,
+      buttonName: "service",
+      left: "/bottom-slider/cart-img2.png",
+      right: "/bottom-slider/img2.png",
+      title: "Lorem ipsum dolor sit amet consectetur",
+      descr:
+        "Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound.",
+    },
+  ];
 
-    return (
-        <div className="w-full mt-[45px] mb-53">
+  return (
+    <div className="mt-[45px] mb-53 w-full">
+      <div className="b-slider-xl:justify-between flex justify-center">
+        {/* left side */}
+        <div className="flex max-h-[582px]">
+          {/* persone */}
+          <img
+            src="/bottom-slider/persone.png"
+            alt="persone"
+            className="b-slider-xl:block hidden"
+          />
+        </div>
 
-            <div className="flex justify-center b-slider-xl:justify-between">
-                {/* left side */}
-                <div className="flex max-h-[582px]">
-                    {/* persone */}
-                    <img src="/bottom-slider/persone.png" alt="persone" className="hidden b-slider-xl:block" />
-                </div>
-
-                {/* right-side */}
-                <div className="relative">
-                    <div className="b-slider-xl:absolute top-[106px] -left-155 w-[741px] shadow-lg rounded-2xl ">
-                        {/* buttons */}
-                        <div className="flex justify-around px-5 italic text-[24px] bg-[#F4C22F] text-white rounded-t-2xl">
-                            {slidesBottom.map((slide, index) => (
-                                <button
-                                    key={slide.buttonName}
-                                    onClick={() => setCurrentSlide(index)}
-                                    className={`flex items-center my-2 cursor-pointer uppercase transition-all duration-300
-                                                ${currentSlide === index ? 'font-semibold border-b-2 border-b-white' : ''}`}
-                                >
-                                    <span className="text-[30px]">{index + 1}.</span>{slide.buttonName}
-                                </button>
-                            ))}
-                        </div>
-
-                        <div className="flex h-[600px] flex-col justify-between items-center bg-white pb-9">
-                            <div className="w-[330px] h-[175px] flex items-center justify-center">
-                                <img src={slidesBottom[currentSlide].left} alt="cart-img" className="mt-[75px] object-cover transition-all duration-300" />
-                            </div>
-
-                            <div className="text-center">
-                                <h3 className="text-center text-[#F4C22F] text-[40px] pt-9 pb-3 uppercase">{slidesBottom[currentSlide].title}</h3>
-                                <p className="px-[72px] text-[#676767] text-[15px]">{slidesBottom[currentSlide].descr}</p>
-                            </div>
-
-                            <ButtonCircle title="learn more" />
-                        </div>
-                    </div>
-
-                    <img 
-                        src={slidesBottom[currentSlide].right} 
-                        alt="right-slide" 
-                        className="w-[741px] h-[400px] b-slider-xl:w-[970px] b-slider-xl:h-[630px] rounded-b-2xl b-slider-xl:rounded-b-none object-cover transition-all duration-300" 
-                    />
-                </div>
+        {/* right-side */}
+        <div className="relative">
+          <div className="b-slider-xl:absolute b-slider-sm:w-[430px] b-slider-md:w-[615px] b-slider-lg:w-[741px] top-[106px] -left-155 w-[350px] rounded-2xl shadow-lg">
+            {/* buttons */}
+            <div className="b-slider-lg:flex-row flex flex-col items-center justify-around rounded-t-2xl bg-[#F4C22F] px-5 text-[24px] text-white italic">
+              {slidesBottom.map((slide, index) => (
+                <button
+                  key={slide.buttonName}
+                  onClick={() => setCurrentSlide(index)}
+                  className={`my-2 flex cursor-pointer items-center uppercase transition-all duration-300 ${currentSlide === index ? "border-b-2 border-b-white font-semibold" : ""}`}
+                >
+                  <span className="text-[30px]">{index + 1}.</span>
+                  {slide.buttonName}
+                </button>
+              ))}
             </div>
 
+            <div className="flex h-[600px] flex-col items-center justify-between bg-white pb-9">
+              <div className="flex h-[175px] w-[330px] items-center justify-center">
+                <img
+                  src={slidesBottom[currentSlide].left}
+                  alt="cart-img"
+                  className="mt-[75px] object-cover transition-all duration-300"
+                />
+              </div>
+
+              <div className="text-center">
+                <h3 className="b-slider-md:text-[40px] pt-9 pb-3 text-center text-[18px] text-[#F4C22F] uppercase">
+                  {slidesBottom[currentSlide].title}
+                </h3>
+                <p className="b-slider-md:px-[72px] text-md b-slider-md:text-[15px] px-5 text-[#676767]">
+                  {slidesBottom[currentSlide].descr}
+                </p>
+              </div>
+
+              <ButtonCircle title="learn more" />
+            </div>
+          </div>
+
+          <img
+            src={slidesBottom[currentSlide].right}
+            alt="right-slide"
+            className="b-slider-sm:w-[430px] b-slider-md:w-[615px] b-slider-lg:w-[741px] b-slider-xl:w-[970px] b-slider-xl:h-[630px] b-slider-xl:rounded-b-none mx-auto h-[400px] w-[350px] rounded-b-2xl object-cover transition-all duration-300"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BottomSlider;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,121 +1,248 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-    const closeMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
+  const closeMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
-    return (
-        <>
-            {/* 1st */}
-            <div className='hidden lg:flex relative justify-between bg-[#F6F6F6] px-[120px]'>
-                {/* logo */}
-                <div>
-                    <img src="/logo.svg" alt="logo" className='absolute top-0 left-[128px] px-[9px] bg-white pt-5 border border-b-8 border-l-8 border-r-8 border-[#F6F6F6] rounded-b-full pb-[12px] z-10' />
+  return (
+    <>
+      {/* 1st */}
+      <div className="relative hidden justify-between bg-[#F6F6F6] px-[120px] lg:flex">
+        {/* logo */}
+        <div>
+          <img
+            src="/logo.svg"
+            alt="logo"
+            className="absolute top-0 left-[128px] z-10 rounded-b-full border border-r-8 border-b-8 border-l-8 border-[#F6F6F6] bg-white px-[9px] pt-5 pb-[12px]"
+          />
 
-                    <div className='absolute top-1 left-[73px] w-18 h-[68px] bg-white border-t-30 border-r-16 border-[#F6F6F6] rounded-tr-full'></div>
-                    <div className='absolute top-1 left-[239px] w-18 h-[68px] bg-white border-t-30 border-l-16 border-[#F6F6F6] rounded-tl-full'></div>
-                    <img src="/flag.png" alt="" className='absolute top-25 left-[165px] -skew-y-6 origin-left rounded-sm' />
-                </div>
+          <div className="absolute top-1 left-[73px] h-[68px] w-18 rounded-tr-full border-t-30 border-r-16 border-[#F6F6F6] bg-white"></div>
+          <div className="absolute top-1 left-[239px] h-[68px] w-18 rounded-tl-full border-t-30 border-l-16 border-[#F6F6F6] bg-white"></div>
+          <img
+            src="/flag.png"
+            alt=""
+            className="absolute top-25 left-[165px] origin-left -skew-y-6 rounded-sm"
+          />
+        </div>
 
-                {/* nav */}
-                <div className='flex gap-px text-white font-semibold z-9'>
-                    <a href="wholesale" className='px-[34px] py-[5px] bg-[#B8B8B8] hover:bg-amber-500 rounded-bl-md uppercase'>Wholesale</a>
-                    <a href="recipes" className='px-[34px] py-[5px] bg-[#B8B8B8] hover:bg-amber-500 uppercase'>Recipes</a>
-                    <a href="blog" className='px-[34px] py-[5px] bg-[#B8B8B8] hover:bg-amber-500 uppercase'>Blog</a>
-                    <a href="faq" className='px-[34px] py-[5px] bg-[#B8B8B8] hover:bg-amber-500 rounded-br-md uppercase'>FAQ</a>
-                </div>
+        {/* nav */}
+        <div className="z-9 flex gap-px font-semibold text-white">
+          <a
+            href="wholesale"
+            className="rounded-bl-md bg-[#B8B8B8] px-[34px] py-[5px] uppercase hover:bg-amber-500"
+          >
+            Wholesale
+          </a>
+          <a
+            href="recipes"
+            className="bg-[#B8B8B8] px-[34px] py-[5px] uppercase hover:bg-amber-500"
+          >
+            Recipes
+          </a>
+          <a
+            href="blog"
+            className="bg-[#B8B8B8] px-[34px] py-[5px] uppercase hover:bg-amber-500"
+          >
+            Blog
+          </a>
+          <a
+            href="faq"
+            className="rounded-br-md bg-[#B8B8B8] px-[34px] py-[5px] uppercase hover:bg-amber-500"
+          >
+            FAQ
+          </a>
+        </div>
+      </div>
+
+      {/* 2nd */}
+      <div className="mt-9 ml-75 hidden flex-col items-center justify-between pr-[120px] lg:flex xl:flex-row">
+        <div className="mb-5 flex gap-8 text-sm text-[#676767] xl:mb-0">
+          <a href="#" className="uppercase">
+            belgian pearl sugar
+          </a>
+          <a href="#" className="uppercase">
+            liege waffle mix
+          </a>
+          <a href="#" className="uppercase">
+            liege waffle maker
+          </a>
+          <a href="#" className="uppercase">
+            speciality ingredients
+          </a>
+        </div>
+
+        <div className="flex items-center gap-[30px] text-[#B8B8B8]">
+          {/* search */}
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search"
+              className="h-[50px] w-40 cursor-pointer rounded-full border pl-11 xl:w-60"
+            />
+            <img
+              src="/search.png"
+              alt="search-icon"
+              className="absolute top-4.5 left-5"
+            />
+          </div>
+
+          {/* basket */}
+          <img src="/basket.png" alt="basket" className="cursor-pointer" />
+
+          {/* sign in */}
+          <div className="cursor-pointer text-[#7E7E7E]">
+            <img
+              src="/signIn.png"
+              alt="sign-in"
+              className="mr-2 inline-block"
+            />{" "}
+            Sign in
+          </div>
+        </div>
+      </div>
+
+      {/* mobile version */}
+      <div className="relative bg-[#F6F6F6] lg:hidden">
+        <div className="relative z-20 flex items-center justify-between bg-[#F6F6F6] px-20 py-4 shadow-sm">
+          {/* mobile-logo */}
+          <div className="flex items-center justify-center">
+            <img src="/logo.svg" alt="logo" className="h-auto w-20" />
+          </div>
+
+          {/* pop-up button */}
+          <button
+            onClick={closeMenu}
+            className="text-gray-600 focus:outline-none"
+          >
+            {isMobileMenuOpen ? (
+              <svg
+                className="h-8 w-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-8 w-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+
+        {/* pop-up */}
+        {isMobileMenuOpen && (
+          <div className="absolute top-full left-0 z-50 flex w-full flex-col gap-6 bg-white p-6 shadow-lg">
+            <div className="relative w-full">
+              <input
+                type="text"
+                placeholder="Search"
+                className="h-[45px] w-full rounded-full border border-gray-200 bg-gray-50 pl-10 outline-none"
+              />
+              <img
+                src="/search.png"
+                alt="search"
+                className="absolute top-3.5 left-4 h-4 w-4 opacity-50"
+              />
             </div>
 
-            {/* 2nd */}
-            <div className='hidden lg:flex flex-col xl:flex-row justify-between items-center ml-75 mt-9 pr-[120px]'>
-                <div className='flex gap-8  text-sm text-[#676767] mb-5 xl:mb-0'>
-                    <a href="#" className='uppercase'>belgian pearl sugar</a>
-                    <a href="#" className='uppercase'>liege waffle mix</a>
-                    <a href="#" className='uppercase'>liege waffle maker</a>
-                    <a href="#" className='uppercase'>speciality ingredients</a>
-                </div>
-
-                <div className='flex items-center gap-[30px] text-[#B8B8B8]'>
-                    {/* search */}
-                    <div className='relative'>
-                        <input type="text" placeholder='Search' className='w-40 xl:w-60 h-[50px] pl-11 border rounded-full cursor-pointer' />
-                        <img src="/search.png" alt="search-icon" className='absolute top-4.5 left-5'  />
-                    </div>
-
-                    {/* basket */}
-                    <img src="/basket.png" alt="basket" className='cursor-pointer' />
-                    
-                    {/* sign in */}
-                    <div className='text-[#7E7E7E] cursor-pointer'>
-                        <img src="/signIn.png" alt="sign-in" className='inline-block mr-2'/> Sign in
-                    </div>
-                </div>
+            <div className="flex flex-col gap-4 text-sm font-bold text-gray-700 uppercase">
+              <a
+                onClick={closeMenu}
+                href="belgian-pearl-sugar"
+                className="hover:text-amber-500"
+              >
+                belgian pearl sugar
+              </a>
+              <a
+                onClick={closeMenu}
+                href="liege-waffle-mix"
+                className="hover:text-amber-500"
+              >
+                liege waffle mix
+              </a>
+              <a
+                onClick={closeMenu}
+                href="liege-waffle-maker"
+                className="hover:text-amber-500"
+              >
+                liege waffle maker
+              </a>
+              <a
+                onClick={closeMenu}
+                href="speciality-ingredients"
+                className="hover:text-amber-500"
+              >
+                speciality ingredients
+              </a>
             </div>
 
-            
+            <hr className="border-gray-100" />
 
-            {/* mobile version */}
-            <div className='lg:hidden relative bg-[#F6F6F6]'>
-                <div className='flex justify-between items-center px-20 py-4 shadow-sm relative z-20 bg-[#F6F6F6]'>
-                    {/* mobile-logo */}
-                    <div className='flex items-center justify-center'>
-                        <img src="/logo.svg" alt="logo" className='w-20 h-auto' />
-                    </div>
-
-                    {/* pop-up button */}
-                    <button 
-                        onClick={closeMenu}
-                        className='text-gray-600 focus:outline-none'
-                    >
-                        {isMobileMenuOpen ? (
-                            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>
-                        ) : (
-                            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
-                        )}
-                    </button>
-                </div>
-
-                {/* pop-up */}
-                {isMobileMenuOpen && (
-                    <div className='absolute top-full left-0 w-full bg-white shadow-lg z-50 flex flex-col p-6 gap-6'>
-                        <div className='relative w-full'>
-                             <input type="text" placeholder='Search' className='w-full h-[45px] pl-10 border border-gray-200 rounded-full bg-gray-50 outline-none' />
-                             <img src="/search.png" alt="search" className='absolute top-3.5 left-4 w-4 h-4 opacity-50' />
-                        </div>
-
-                        <div className='flex flex-col gap-4 text-gray-700 font-bold text-sm uppercase'>
-                            <a onClick={closeMenu} href="belgian-pearl-sugar" className='hover:text-amber-500'>belgian pearl sugar</a>
-                            <a onClick={closeMenu} href="liege-waffle-mix" className='hover:text-amber-500'>liege waffle mix</a>
-                            <a onClick={closeMenu} href="liege-waffle-maker" className='hover:text-amber-500'>liege waffle maker</a>
-                            <a onClick={closeMenu} href="speciality-ingredients" className='hover:text-amber-500'>speciality ingredients</a>
-                        </div>
-                        
-                        <hr className='border-gray-100' />
-
-                        <div className='flex flex-wrap gap-3 text-xs font-semibold text-white'>
-                            <a onClick={closeMenu} href="wholesale" className='px-4 py-2 bg-[#B8B8B8] rounded-md'>Wholesale</a>
-                            <a onClick={closeMenu} href="recipes" className='px-4 py-2 bg-[#B8B8B8] rounded-md'>Recipes</a>
-                            <a onClick={closeMenu} href="blog" className='px-4 py-2 bg-[#B8B8B8] rounded-md'>Blog</a>
-                            <a onClick={closeMenu} href="faq" className='px-4 py-2 bg-[#B8B8B8] rounded-md'>FAQ</a>
-                        </div>
-
-                        {/* Sign In & Basket */}
-                        <div className='flex justify-between items-center mt-2'>
-                            <div className='text-[#7E7E7E] font-semibold flex items-center gap-2'>
-                                <img src="/signIn.png" alt="sign-in" className='w-5'/> Sign in
-                            </div>
-                            <div className='flex items-center gap-2 text-[#7E7E7E]'>
-                                <img src="/basket.png" alt="basket" className='w-6' /> Basket
-                            </div>
-                        </div>
-                    </div>
-                )}
+            <div className="flex flex-wrap gap-3 text-xs font-semibold text-white">
+              <a
+                onClick={closeMenu}
+                href="wholesale"
+                className="rounded-md bg-[#B8B8B8] px-4 py-2"
+              >
+                Wholesale
+              </a>
+              <a
+                onClick={closeMenu}
+                href="recipes"
+                className="rounded-md bg-[#B8B8B8] px-4 py-2"
+              >
+                Recipes
+              </a>
+              <a
+                onClick={closeMenu}
+                href="blog"
+                className="rounded-md bg-[#B8B8B8] px-4 py-2"
+              >
+                Blog
+              </a>
+              <a
+                onClick={closeMenu}
+                href="faq"
+                className="rounded-md bg-[#B8B8B8] px-4 py-2"
+              >
+                FAQ
+              </a>
             </div>
-        </>
-    );
+
+            {/* Sign In & Basket */}
+            <div className="mt-2 flex items-center justify-between">
+              <div className="flex items-center gap-2 font-semibold text-[#7E7E7E]">
+                <img src="/signIn.png" alt="sign-in" className="w-5" /> Sign in
+              </div>
+              <div className="flex items-center gap-2 text-[#7E7E7E]">
+                <img src="/basket.png" alt="basket" className="w-6" /> Basket
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
